517abd136f29f80a0821b9809538015ce67469af
Matthew Richardson <m.richardson@ed.ac.uk>
2015-12-20 21:40:15 +0000

    Extra features: - handle saving history file at exit (also need to load at start) - add ipc via socket for client listing and adding to clipboard - Make it a class - all sorts of tidying, hacking, debug statements etc.

--- clipster.py
+++ clipster.py
@@ -1,13 +1,13 @@
-from gi.repository import Gtk, Gdk, GLib
+from gi.repository import Gtk, Gdk, GLib, GObject
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 import time
 import argparse
 import json
-
-
-boards = {"primary": [],
-               "secondary": []}
+import atexit
+import socket
+import os
+import select
 
 class Clips(Gtk.Clipboard):
 
@@ -20,13 +20,19 @@ class Clips(Gtk.Clipboard):
 class Clipster(object):
     def __init__(self):
         self.clips = Clips()
-	self.boards = {"primary": [], "secondary": []}
-	self.window = self.pid = self.cid = None
+        self.boards = {"primary": [], "clipboard": []}
+        self.window = self.pid = self.cid = None
+        self.histfile = "/home/mrichar1/.clipster_history"
+        atexit.register(self.write_history_file)
+
+    def write_history_file(self):
+        with open(self.histfile, 'w') as f:
+            json.dump(self.boards, f)
 
     def read_from_clip(self, board, _, board_type):
         if board_type == "primary":
 	    event_id = self.pid
-        else:    
+        else:
             event_id = self.cid
         # Some apps update primary during mouse drag (chrome)
         # Block at start to prevent repeated triggering
@@ -39,19 +45,60 @@ class Clipster(object):
         temp = board.wait_for_text()
         if temp:
 	    # Rempve existing item (before adding to end)
-	    if temp in boards[board_type]:
-	        boards[board_type].remove(temp)
-	    boards[board_type].append(temp)
-	print(boards[board_type])
+	    if temp in self.boards[board_type]:
+	        self.boards[board_type].remove(temp)
+	    self.boards[board_type].append(temp)
+	print(self.boards[board_type])
         # Unblock event handling
 	board.handler_unblock(event_id)
 
-    def daemon():
+    def create_sock(self):
+        # FIXME: better place for this
+        sock_address = "/tmp/.clipster_sock"
+        try:
+            os.unlink(sock_address)
+        except OSError: # FIXME: use better io error handling than this
+            if os.path.exists(sock_address):
+                raise
+
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.setblocking(0)
+        self.sock.bind(sock_address)
+        self.sock.listen(5)
+        self.inputs = [self.sock]
+
+
+    def ipc(self, sock, g_flags):
+        conn, client_address = sock.accept()
+        conn.setblocking(0)
+        data = []
+        while True:
+            try:
+                recv = conn.recv(8192)
+                if not recv:
+                    break
+                data.append(recv)
+            except socket.error:
+                break
+        if data:
+            sent = ''.join(data)
+            if sent.startswith("LIST:"):
+                conn.sendall(json.dumps(self.boards))
+            else:
+                self.clips.primary.set_text(sent, len=-1)
+                print("RCVD:", sent)
+        conn.close()
+        return True
+
+
+    def daemon(self):
         # Create a Window class instance, as creating a Display()
         # instance and calling get_pointer() from it segfaults
         self.window = Gtk.Window(type=Gtk.WindowType.POPUP)
         self.pid = self.clips.primary.connect('owner-change', self.read_from_clip, "primary")
         self.cid = self.clips.clipboard.connect('owner-change', self.read_from_clip, "clipboard")
+        self.create_sock()
+        GObject.io_add_watch(self.sock, GObject.IO_IN, self.ipc)
         Gtk.main()
 
 
@@ -69,7 +116,3 @@ if __name__ == "__main__":
 
     if args.list:
         print(' '.join(boards['primary']))
-
-
-
-
