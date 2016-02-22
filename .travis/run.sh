25cd655167b3de49d8d8788a4fa864f3307c3b38
Matthew Richardson <M.Richardson@ed.ac.uk>
2016-02-22 12:12:27 +0000

    Use py2 pylint/pep8 until travis supports others.

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,13 +3,16 @@
 set -e
 set -x
 
-if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
-  PEP8="python3 /usr/lib/python3/dist-packages/pep8.py"
-  PYLINT=/usr/bin/pylint3
-else
+# Commented out py3 specific apps until #2442/3 resolved:
+# https://github.com/travis-ci/apt-package-whitelist/issues
+#if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
+#  PEP8="python3 /usr/lib/python3/dist-packages/pep8.py"
+#  PYLINT=/usr/bin/pylint3
+#else
   PEP8=/usr/bin/pep8
   PYLINT=/usr/bin/pylint
-fi
+#fi
+
 # Ignore imports not at start and line-too-long)
 $PEP8 --ignore=E402,E501 clipster
 $PYLINT --errors-only clipster
