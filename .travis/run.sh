31eeb73be44eca75d05748cb2daf74a9496c3248
Matthew Richardson <M.Richardson@ed.ac.uk>
2016-02-22 12:29:20 +0000

    Try using pylint and pep8 via pip

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,19 +3,8 @@
 set -e
 set -x
 
-# Commented out py3 specific apps until #2442/3 resolved:
-# https://github.com/travis-ci/apt-package-whitelist/issues
-#if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
-#  PEP8="python3 /usr/lib/python3/dist-packages/pep8.py"
-#  PYLINT=/usr/bin/pylint3
-#else
-  PEP8=/usr/bin/pep8
-  PYLINT=/usr/bin/pylint
-#fi
-
-# Commented out pep8 until above bug is fixed.
 # Ignore imports not at start and line-too-long)
-#$PEP8 --ignore=E402,E501 clipster
-$PYLINT --errors-only clipster
+pep8 --ignore=E402,E501 clipster
+pylint --errors-only clipster
 
 python tests/tests.py
