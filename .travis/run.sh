c7d429178adaf874a42d570b12fc2006c7a9fba8
Matthew Richardson <M.Richardson@ed.ac.uk>
2016-02-22 10:01:40 +0000

    Add pep8 and pylint to tests.

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,4 +3,15 @@
 set -e
 set -x
 
+if [[ $TRAVIS_PYTHON_VERSION == 3* ]]; then
+  PEP8="python3 /usr/lib/python3/dist-packages/pep8.py"
+  PYLINT=/usr/bin/pylint3
+else:
+  PEP8=/usr/bin/pep8
+  PYLINT=/usr/bin/pylint
+fi
+# Ignore imports not at start and line-too-long)
+$PEP8 --ignore=E402,E501 clipster
+$PYLINT --errors-only clipster
+
 python tests/tests.py
