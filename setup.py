e3240e57b765ec0d3c0c20c4b13d1f49f867f6e6
jakeogh <github.com@v6y.net>
2017-05-20 12:23:07 -0700

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+clipster - python clipboard manager
+"""
+
+from setuptools import find_packages, setup
+
+dependencies = ['pygobject']
+
+version = 0.01
+
+setup(
+    name = "clipster",
+    version = version,
+    url = "https://github.com/jakeogh/kcl",
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    author = "mrichar1",
+    author_email = "",
+    description='python clipboard manager',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
